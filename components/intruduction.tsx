10bf4f5566847a8a82a381919de29e547d2f41e6
Loki <yongxiang.zhan@outlook.com>
2026-02-05 21:12:43 +0800

    Update Intruduction component styles to include horizontal padding

--- components/intruduction.tsx
+++ components/intruduction.tsx
@@ -1,6 +1,6 @@
 export function Intruduction() {
   return (
-    <div className="flex flex-col items-center w-dvw min-h-dvh py-32">
+    <div className="flex flex-col items-center w-dvw min-h-dvh py-32 px-4">
       <div className="max-w-6xl w-full mx-auto">
         <h1 className="text-6xl pb-4">Hi, I'm <strong className="font-bold">Yong-Xiang Zhan.</strong></h1>
         <p className="text-4xl font-light"></p>
